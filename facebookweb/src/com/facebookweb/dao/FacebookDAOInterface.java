50c369e3220febbad8a3e2436ab2b49ba3227fca
UT55684 <UT55684@AMSW56SLPC0120.ind.zensar.com>
2019-09-06 12:42:07 +0530

    manas

--- facebookweb/src/com/facebookweb/dao/FacebookDAOInterface.java
+++
@@ -1,9 +0,0 @@
-package com.facebookweb.dao;
-
-import com.facebookweb.entity.FacebookEmployee;
-
-public interface FacebookDAOInterface {
-
-	int createProfileDao(FacebookEmployee fe);
-
-}
--- facebookweb/src/com/facebookweb/service/FacebookServiceInterface.java
+++
@@ -1,9 +0,0 @@
-package com.facebookweb.service;
-
-import com.facebookweb.entity.FacebookEmployee;
-
-public interface FacebookServiceInterface {
-
-	int createProfileService(FacebookEmployee fe);
-
-}
